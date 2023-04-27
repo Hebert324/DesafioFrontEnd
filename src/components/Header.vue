37758518150d801979b30c6e8ad31a60706b097d
Hebert Rocha <70274409+Hebert324@users.noreply.github.com>
2023-04-26 22:01:47 -0300

    feat(Header): Cria componente Header

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <img src="../assets/logo.svg" alt="Logo Ricky and Morty" />
+    <a rel="stylesheet" href="#">
+      <img src="../assets/avatar.svg" alt="Icone de um avatar de usuario" />
+    </a>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.9rem 1.6rem;
+  background: rgb(218, 242, 244);
+
+  background: linear-gradient(
+    90deg,
+    #f1f3fa 0%,
+    rgba(242, 244, 250, 0.652771) 34.72%,
+    rgb(218, 242, 244) 100%
+  );
+
+  cursor: pointer;
+}
+</style>
