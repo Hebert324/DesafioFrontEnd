565810bc56c2b2feb7c13fc21050aa8de16fc431
Hebert Rocha <hebertrocha76@gmail.com>
2023-04-27 12:38:26 -0300

    feat(Main): Cria componente Main e adiciona a columa de informações

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import Header from "./components/Header.vue";
 import Middle from "./components/Middle.vue";
+import Main from "./components/Main.vue";
 </script>
 
 <template>
   <Header />
   <Middle />
+  <Main />
 </template>
-
-<style scoped></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,59 @@
+<template>
+  <main>
+    <div class="column-infos">
+      <div>
+        <input type="checkbox" />
+        <p>Name</p>
+      </div>
+      <p>Status</p>
+      <p>Species</p>
+      <p>Gender</p>
+      <p>Origin</p>
+      <p>Location</p>
+      <p>Link</p>
+      <span><p>Creation Date</p></span>
+    </div>
+    <div class="list-personagens"></div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  margin-left: 7.2rem;
+}
+
+.column-infos {
+  padding: 0.8rem 0.4rem 0 1rem;
+}
+
+.column-infos input {
+  margin: 1rem 0rem 1rem 1.2rem;
+}
+
+.column-infos div {
+  display: flex;
+  align-items: center;
+  gap: 2.4rem;
+}
+
+.column-infos p {
+  font-weight: 500;
+  font-size: 1.2rem;
+  line-height: 1.5rem;
+  letter-spacing: -0.03em;
+
+  color: #81828a;
+
+  margin: 0 2.4rem;
+}
+
+.column-infos span p {
+  margin-right: 0.4rem;
+}
+
+.column-infos {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
