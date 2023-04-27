d459fdd5e98e40818fb5b7c70596070915e15b74
Hebert Rocha <70274409+Hebert324@users.noreply.github.com>
2023-04-27 06:36:11 -0300

    feat(Middle): Adiciona componente de Middle. OBS: ainda sem estilização

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from "./components/Header.vue";
+import Middle from "./components/Middle.vue";
 </script>
 
 <template>
   <Header />
+  <Middle />
 </template>
 
 <style scoped></style>
---
+++ src/components/Middle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="header-personagens">
+    <img
+      src="../assets/btn-add-personagens.svg"
+      alt="Botão verde de adicionar"
+    />
+    <p>Personagens</p>
+  </div>
+  <div class="menu">
+    <div class="pesquisar">
+      <img
+        src="../assets/btn-search.svg"
+        alt="Botão para Pesquisar ou Buscar personagens"
+      />
+      <p>Buscar</p>
+    </div>
+    <div class="button-atualizar">
+      <img src="../assets/btn-refreseh.svg" alt="Botão de atualizar a página" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
