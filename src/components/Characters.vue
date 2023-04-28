902efcfe1872fa42aafc7aab3235bc05e58774d2
Hebert Rocha <hebertrocha76@gmail.com>
2023-04-28 13:15:29 -0300

    refactor(Characters): Refato código, modifica para uso de tabelas

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,28 +1,10 @@
 <script setup props="props">
 import { ref, onMounted } from "vue";
+import { fetchApi, formatDate } from "../services/api.js";
 
 const props = defineProps({
-  id: String,
+  id: Number,
 });
-
-const formatDate = (dateString) => {
-  const date = new Date(dateString);
-  const day = date.getDate().toString().padStart(2, "0");
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const year = date.getFullYear();
-
-  return `${day}/${month}/${year}`;
-};
-
-const fetchApi = (id) => {
-  const result = fetch(`https://rickandmortyapi.com/api/character/${id}`)
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
-
-  return result;
-};
 
 let img = ref("");
 let name = ref("");
@@ -34,61 +16,71 @@ let location = ref("");
 let characterLink = ref("");
 let creationDate = ref("");
 
-const fetchCharacter = () => {
-  fetchApi(props.id)
-    .then((character) => {
-      name.value = character.name;
-      status.value = character.status;
-      species.value = character.species;
-      gender.value = character.gender;
-      origin.value = character.origin.name;
-      location.value = character.location.name;
-      characterLink.value = character.url;
-      creationDate.value = formatDate(character.created);
-      img.value = character.image;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+const fetchCharacter = async () => {
+  try {
+    const character = await fetchApi(props.id);
+    name.value = character.name;
+    status.value = character.status;
+    species.value = character.species;
+    gender.value = character.gender;
+    origin.value = character.origin.name;
+    location.value = character.location.name;
+    characterLink.value = character.url;
+    creationDate.value = formatDate(character.created);
+    img.value = character.image;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 onMounted(fetchCharacter);
 </script>
 
 <template>
-  <div class="characters">
-    <div>
-      <img class="characters-img" v-bind:src="img" alt="Foto do Personagem" />
-      <p id="name">{{ name }}</p>
-    </div>
-    <div>
-      <img
-        class="rocket"
-        src="../assets/rocket.svg"
-        alt="Imagem de um foguete"
-      />
-      <p class="status">{{ status }}</p>
-    </div>
-    <p class="species">{{ species }}</p>
-    <p class="gender">{{ gender }}</p>
-    <p class="origin">{{ origin.slice(0, 10) + "..." }}</p>
-    <p class="location">{{ location.slice(0, 10) + "..." }}</p>
-    <a v-bind:href="characterLink" target="_blank">Character link</a>
-    <p class="creation-date">{{ creationDate }}</p>
-  </div>
+  <tbody>
+    <tr>
+      <td>
+        <img class="characters-img" v-bind:src="img" alt="Foto do Personagem" />
+      </td>
+      <td id="name">{{ name }}</td>
+      <td>
+        <img
+          class="rocket"
+          src="../assets/rocket.svg"
+          alt="Imagem de um foguete"
+        />
+      </td>
+      <td class="status">{{ status }}</td>
+      <td class="species">{{ species }}</td>
+      <td class="gender">{{ gender }}</td>
+      <td class="origin">{{ origin }}</td>
+      <td class="location">{{ location }}</td>
+      <td>
+        <a v-bind:href="characterLink" target="_blank">Character link</a>
+      </td>
+      <td class="creation-date">{{ creationDate }}</td>
+    </tr>
+  </tbody>
+  <br />
 </template>
 
 <style scoped>
-.characters {
+tr td {
   background-color: #ffffff;
+}
+
+td {
+  font-size: 1.3rem;
+  line-height: 1.6rem;
+  letter-spacing: -0.03em;
+
+  color: #696a72;
+
   padding: 1rem;
-  border-radius: 26px 0px 0px 26px;
+}
 
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  margin-bottom: 0.1rem;
+tr td:first-child {
+  border-radius: 2.6rem 0 0 2.6rem;
 }
 
 .characters-img {
@@ -97,16 +89,7 @@ onMounted(fetchCharacter);
   border-radius: 99px;
 }
 
-.characters p {
-  font-weight: 500;
-  font-size: 1.3rem;
-  line-height: 1.6rem;
-  letter-spacing: -0.03em;
-
-  color: #696a72;
-}
-
-.characters a {
+a {
   font-weight: 600;
   font-size: 1rem;
   line-height: 1.2rem;
@@ -128,12 +111,6 @@ onMounted(fetchCharacter);
   letter-spacing: -0.03em;
 
   color: #3a3c43;
-}
-
-.characters div {
-  display: flex;
-  align-items: center;
-  gap: 2.4rem;
 }
 
 .rocket {
