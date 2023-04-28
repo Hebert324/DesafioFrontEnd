7880b8af4438a0b5c609e39ad08a425582bc1a1e
Hebert Rocha <hebertrocha76@gmail.com>
2023-04-28 13:18:17 -0300

    feat(ColumnInfos): Cria novo componente para separar a coluna de informação do Main

---
+++ src/components/ColumnInfos.vue
@@ -0,0 +1,40 @@
+<template>
+  <thead>
+    <tr>
+      <th class="input"><input type="checkbox" /></th>
+      <th>Name</th>
+      <th></th>
+      <th>Status</th>
+      <th>Species</th>
+      <th>Gender</th>
+      <th>Origin</th>
+      <th>Location</th>
+      <th>Link</th>
+      <th>Creation Date</th>
+    </tr>
+  </thead>
+</template>
+
+<style scoped>
+input {
+  margin: 1rem 2.4rem 1rem 1.2rem;
+  background: #81828a;
+}
+
+tr th:first-child {
+  text-align: center;
+}
+
+th {
+  font-weight: 500;
+  font-size: 1.2rem;
+  line-height: 1.5rem;
+  letter-spacing: -0.03em;
+
+  color: #81828a;
+
+  padding: 0.8rem 0.4rem 0.8rem 0rem;
+
+  text-align: left;
+}
+</style>
